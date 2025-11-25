944e8013872d09c1d39b47106e0c13e12597bfcc
TasyDev <tamayocontat@gmail.com>
2025-11-24 19:32:45 -0500

    Update delete page layout and product card width
    Refactored the delete page HTML to use a two-column layout, separating the product list and an empty sidebar. Adjusted product card width from col-5 to col-4 for improved display within the new layout.

--- src/views/pages/delete/delete.js
+++ src/views/pages/delete/delete.js
@@ -6,7 +6,7 @@ const getElements = document.querySelector(".get");
 getProductsAll().then(products => {
     for (const product of products) {
         const html = `
-<div class="col-5 col-md-2 p-2 bg-fourth-p rounded-4 mt-3 mx-md-5 mx-2
+<div class="col-4 col-md-2 p-2 bg-fourth-p rounded-4 mt-3 mx-md-5 mx-2
      d-flex flex-column align-items-center text-center">
     <img src="${product.images}" 
          alt="${product.id}" 
